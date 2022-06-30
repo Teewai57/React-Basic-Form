668daf1244b7b1da126a473c08ac5a97ed1facde
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-30 14:45:57 +0100

    ..

--- src/Modal.js
+++ src/Modal.js
@@ -16,7 +16,7 @@ const style = {
 };
 
 export default function BasicModal(props) {
-  console.log(props)
+    console.log(props)
   const [open, setOpen] = React.useState(true);
   const handleClose = () => setOpen(false);
 
@@ -33,11 +33,13 @@ export default function BasicModal(props) {
             Text in a modal
           </Typography> */}
           <div id="modal-modal-description" sx={{ mt: 2 }}>
-            Welcome {props.name}!
+            <div>
+              Welcome {props.name}!
+              
+            </div>
+            
           </div>
-          <div class="wrapper"> <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /> <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-          </svg>
-          </div>
+          
         </Box>
       </Modal>
     </div>
